a1a1ee32397b04437f435f2a5170d6e8f43f8caf
almatkai <kairatovalmat2003@gmail.com>
2023-08-08 13:30:41 +0600

    Initial Commit

---
+++ Animated Sticky Header/Animated_Sticky_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_Sticky_HeaderApp.swift
+//  Animated Sticky Header
+//
+//  Created by Almat Kairatov on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Animated_Sticky_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated Sticky Header/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Animated Sticky Header
+//
+//  Created by Almat Kairatov on 08.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
